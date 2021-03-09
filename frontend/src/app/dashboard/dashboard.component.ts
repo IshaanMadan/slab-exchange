dbc5f72c9509fb24668599c0b0565581f94c6b74
vikasverma24 <vikas.verma@oodlestechnologies.com>
2021-03-09 13:18:55 +0530

    Added dashboard Io

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,6 +10,16 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // $('.collapse').collapse()
+    $("#open-all").click(function(){
+      console.log("working")
+      $(".input-arrow").prop("checked", true);
+  });
+  $("#side-nav").click(function(){
+    $(".side-upload ").toggle(500)
+  });
   }
+ 
+  
 
 }
