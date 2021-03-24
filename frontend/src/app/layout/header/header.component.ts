a895c1a446a90336c78d9297dd36f6b546f32b3f
Yash <yash.chouriya@oodlestechnologies.com>
2021-03-24 20:17:51 +0530

    bug fixes

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DataService } from 'src/app/_providers/data.service';
@@ -7,30 +7,45 @@ import { FacebookService } from 'src/app/_providers/facebook.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+  styleUrls: ['./header.component.less'],
 })
 export class HeaderComponent implements OnInit {
-
-  userSubscription: Subscription
+  userSubscription: Subscription;
   userData: any;
+  public innerWidth: any;
+  userName;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.nameResize();
+  }
 
   constructor(
     private fbService: FacebookService,
     private dataService: DataService,
     private router: Router
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.userSubscription = this.dataService.userDetails.subscribe(user => {
-      if(user == null) {
-        this.router.navigate(['/'])
+    this.userSubscription = this.dataService.userDetails.subscribe((user) => {
+      if (user == null) {
+        this.router.navigate(['/']);
       }
       this.userData = user;
     });
+    this.innerWidth = window.innerWidth;
+    this.nameResize();
   }
 
   logout() {
     this.fbService.logout();
   }
 
+  nameResize() {
+    this.userName =
+      this.innerWidth < 576
+        ? this.userData.name.split(' ')[0]
+        : this.userData.name;
+  }
 }
